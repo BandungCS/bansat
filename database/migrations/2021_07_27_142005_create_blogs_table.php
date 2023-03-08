4d96d90d23650c62c881758f242a171e577039a0
bcs_01 <52460802+BandungCS@users.noreply.github.com>
2023-03-08 21:03:55 +0700

    Add files via upload

---
+++ database/migrations/2021_07_27_142005_create_blogs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('nama');
+            $table->string('noinv');
+            $table->string('kelas');
+            $table->string('tahun');
+            $table->string('kondisi');
+            $table->string('kantor');
+            $table->string('gudang');
+            $table->string('aula');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
